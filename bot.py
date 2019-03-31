d6ca08bee191ccc7a4b5c32ccb74d30b417714c9
ntcrckr <alexei.pozdnyakov@mail.ru>
2019-03-31 20:11:43 +0300

    19th bot

--- bot.py
+++ bot.py
@@ -43,7 +43,7 @@ def send_welcome(message):
 
 
 def register_name_step(message):
-  if message.text = None:
+  if message.text == None:
     msg = bot.send_message(message.chat.id, "Что-то я не вижу названия...")
     bot.register_next_step_handler(msg, register_name_step)
   send_info(message, "name")
